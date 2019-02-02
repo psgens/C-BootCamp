24943aa03a073fc2919a4ef13aab506135a7ea9b
psgens <42700532+psgens@users.noreply.github.com>
2019-02-02 10:17:36 -0600

    Add files via upload

---
+++ Array List/Array List/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring an arraylist with undefined amount of objects
+            ArrayList myArrayList = new ArrayList();
+            //declaring an arraylist with defined amount of objects
+            ArrayList myArrayList2 = new ArrayList(100);
+
+            myArrayList.Add(25);
+            myArrayList.Add("Hello");
+            myArrayList.Add(22.37);
+            myArrayList.Add(7);
+
+            //delete elemet with specific value from the array list
+            myArrayList.Remove(25);
+
+            //delete element at a specific index
+            myArrayList.RemoveAt(2);
+
+            Console.WriteLine(myArrayList.Count);
+
+
+            double sum = 0;
+
+            foreach (object obj in myArrayList)
+            {
+                if (obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }
+                else if (obj is double)
+                {
+                    sum += (double)obj;
+                }
+                else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+            
+
+
+
+
+        }
+    }
+}
---
+++ inheritance/inheritance/VideoPost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class VideoPost:Post
+    {
+        protected string VideoURL { get; set; }
+        protected int Lengthy { get; set; }
+
+
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, int length, bool isPublic)
+        {
+            // The following properties and the GetNextID method ar inherited from Post.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+            // Property VideoURL is a member of ImagePost, but not of Post.
+            this.VideoURL = videoURL;
+            this.Lengthy = length;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} -{2} Length: {3} - by {4}", this.ID, this.Title, this.VideoURL, this.Lengthy, this.SendByUsername);
+        }
+    }
+}
