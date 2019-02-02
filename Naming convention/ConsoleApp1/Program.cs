a353132bf12755a39f856a5adf5feac73532d1c6
psgens <42700532+psgens@users.noreply.github.com>
2019-02-02 10:32:12 -0600

    Add files via upload

---
+++ Naming convention/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingConvention
+{
+    class Program //Class name like Client activity
+    {
+        //Method name like CalculateValue
+        //Method arguments like firstNumber
+        static void Main(string[] args)
+        {
+            //Local variables like itemCount
+
+            //Use userControl instead of usrCtr
+
+            //Don't use numbers at the start of variable names.
+            //Avoid underscore in variable names.
+            
+            //Avoid
+            //String myName
+            //Int32
+            //Boolean
+        }
+    }
+}
---
+++ Strings/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Patrick"; //S in string must be lowercase.
+
+            string message = "My name is " + myname;
+
+            string capmessage = message.ToUpper();
+
+            Console.WriteLine("My name is " + myname);
+            Console.WriteLine(capmessage);
+            Console.Read();
+        }
+    }
+}
